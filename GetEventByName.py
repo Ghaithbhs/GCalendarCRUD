dea5840ef9e161f82626674b60f48169b44c6ee2
Ghaith <ghaith_bhs@yahoo.fr>
2019-06-24 11:38:33 +0100

    24/06/2019

---
+++ GetEventByName.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def main():
+
+    # Get inputs
+    print("What's the name of the event?")
+    n = input()
+    '''print('what the day of the event')
+    d = input()
+    print("month: ")
+    m = input()
+    print('year : ')
+    y = input()
+    print('attendees : ')
+    a = input()
+    print('Meeting Room : ')
+    r = input()
+    maxattendees = 10'''
+    '''if r == "Midoun meeting room":
+        room = "focus-corporation.com_3436373433373035363932@resource.calendar.google.com"
+    elif r == "Aiguilles Meeting Room":
+        room = "focus-corporation.com_3132323634363237333835@resource.calendar.google.com"
+    elif r == "Barrouta Meeting Room":
+        room = "focus-corporation.com_3335353934333838383834@resource.calendar.google.com"
+    elif r == "Kantaoui Meeting Room":
+        room = "focus-corporation.com_3335343331353831343533@resource.calendar.google.com"
+    elif r == "Gorges Meeting Room":
+        room = "focus-corporation.com_3436383331343336343130@resource.calendar.google.com"
+    elif r == "Ichkeul Meeting Room":
+        room = "focus-corporation.com_36323631393136363531@resource.calendar.google.com"
+    elif r == "Khemir Meeting Room":
+        room = "focus-corporation.com_3935343631343936373336@resource.calendar.google.com"
+    elif r == "Tamaghza Meeting Room":
+        room = "focus-corporation.com_3739333735323735393039@resource.calendar.google.com"
+    elif r == "Friguia Meeting Room":
+        room = "focus-corporation.com_3132343934363632383933@resource.calendar.google.com"
+        maxattendees = 15
+    elif r == "Ksour Meeting Room":
+        room = "focus-corporation.com_@resource.calendar.google.com"
+    elif r == "Medeina Meeting Room":
+        room = "focus-corporation.com_@resource.calendar.google.com"
+    elif r == "Thyna Meeting Room":
+        room = "focus-corporation.com_@resource.calendar.google.com"'''
+
+    l = "FOCUS-1ere-Midoune Meeting Room (10)"
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    #Add an event
+    event = {
+        'summary': n,
+    }
+
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=1, singleEvents=True,
+                                          orderBy='startTime', query='name', body=event).execute()
+    events = events_result.get('items', [])
+
+
+if __name__ == '__main__':
+    main()
